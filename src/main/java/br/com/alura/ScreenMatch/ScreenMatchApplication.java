69e3a4b4c049e88fa31c79e42e838d8238592fc6
Enzo <enzopyton24@gmail.com>
2026-05-04 19:33:30 -0300

    obtendo dados do episodio

--- src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.ScreenMatch;
 
+import br.com.alura.ScreenMatch.model.DadosEpisodio;
 import br.com.alura.ScreenMatch.model.DadosSerie;
 import br.com.alura.ScreenMatch.services.ConsumoApi;
 import br.com.alura.ScreenMatch.services.ConverteDados;
@@ -20,19 +21,22 @@ public class ScreenMatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
 		var API_KEY = "&apikey=ce09a206";
-		Scanner input = new Scanner(System.in);
-
-		System.out.println("Digite o filme que quer ver as informações");
-		String filme = input.nextLine();
-		var endereco = "https://www.omdbapi.com/?t=" + filme.replace(" ", "+") + API_KEY;
+		var serie = "Breaking Bad";
+		var endereco = "https://www.omdbapi.com/?t=" + serie.replace(" ", "+") + API_KEY;
 
 		var json = consumoApi.obterDados(endereco);
 		System.out.println(json);
+
 		ConverteDados converser = new ConverteDados();
 		DadosSerie dados = converser.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		
+		// Corrigido o replace para colocar "+" no lugar dos espaços
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=" + dados.titulo().replace(" ", "+") + "&Season=1&episode=3" + API_KEY);
+		DadosEpisodio dadosEpisodio = converser.obterDados(json, DadosEpisodio.class);
 
-		input.close();
+		System.out.println(dadosEpisodio);
+
 	}
 
 }
---
+++ src/main/java/br/com/alura/ScreenMatch/model/DadosEpisodio.java
@@ -0,0 +1,14 @@
+package br.com.alura.ScreenMatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodio(@JsonAlias("Title")String titulo,
+
+                            @JsonAlias("Episode") Integer nunero,
+
+                            @JsonAlias("imdbRating") String avaliacao,
+
+                            @JsonAlias("Released") String dataLancamento) {
+}
