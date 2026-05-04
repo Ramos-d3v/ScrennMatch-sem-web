11a1476d45b972e498dc7976b3fd78aee22a88f4
Enzo <enzopyton24@gmail.com>
2026-05-04 02:39:48 -0300

    feat: user interation on cli

--- src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ScreenMatchApplication implements CommandLineRunner {
 
@@ -17,11 +19,20 @@ public class ScreenMatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=ce09a206");
+		var API_KEY = "&apikey=ce09a206";
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Digite o filme que quer ver as informações");
+		String filme = input.nextLine();
+		var endereco = "https://www.omdbapi.com/?t=" + filme.replace(" ", "+") + API_KEY;
+
+		var json = consumoApi.obterDados(endereco);
 		System.out.println(json);
 		ConverteDados converser = new ConverteDados();
 		DadosSerie dados = converser.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		input.close();
 	}
 
 }
